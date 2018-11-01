d095b2e45e0e076cbc156e462935b00698290216
rela <rela@AppledeMacBook-Pro.local>
2018-11-01 17:56:09 +0800

    test

--- algo/quick_match/algos.go
+++ algo/quick_match/algos.go
@@ -1,6 +1,7 @@
 package quick_match
 
 import (
+	"fmt"
 	"os"
 )
 
@@ -9,6 +10,7 @@ var MatchAlgo QuickMatchTree
 func init() {
 	Work_dir, _ = os.Getwd()
 	model_file := Work_dir + "/algo_files/quick_match_tree.model"
+	fmt.Println(model_file)
 	MatchAlgo = QuickMatchTree{FilePath: model_file}
 	MatchAlgo.Init()
 }
