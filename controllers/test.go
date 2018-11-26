1f9cff23d3286b45e48963a5a5e8139c9dd9a110
rela <rela@ArvindeMacBook-Pro.local>
2018-11-26 17:46:48 +0800

    test

---
+++ controllers/test.go
@@ -0,0 +1,31 @@
+package controllers
+
+import (
+	"time"
+	"rela_recommend/factory"
+	"rela_recommend/models/mongo"
+	"rela_recommend/routers"
+	"math/rand"
+	"rela_recommend/log"
+)
+
+
+func TestHTTP(c *routers.Context) {
+	var startTime = time.Now()
+	// var params MatchRecommendReqParams
+
+	var mongoClient = factory.MatchClusterMon.Copy()
+	defer mongoClient.Close()
+
+	var maxId int64 = 415285405006916
+	userId := rand.Int63n(maxId)
+	userIds := make([]int64, 0)
+	for i:=0;i<3000;i++{
+		userIds = append(userIds, rand.Int63n(maxId))
+	}
+	aulm := mongo.NewActiveUserLocationModule(mongoClient)
+	aulm.QueryByUserAndUsers(userId, userIds)
+	var startLogTime = time.Now()
+	log.Infof("Test:userids:%d,total:%.3f", len(userIds), startLogTime.Sub(startTime).Seconds())
+}
+
