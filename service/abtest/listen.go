a973ac717a5b4165c55248215925adba2400669e
rela <rela@ArvindeMBP.lan>
2019-04-02 11:38:19 +0800

    add live test v2

--- service/abtest/listen.go
+++ service/abtest/listen.go
@@ -50,7 +50,7 @@ func init() {
 			{"name": "match_model", "desc": "匹配模型", "app": "match", "ids":[104708381],"factor_map":{"match_model":"QuickMatchTreeV1_4"}}
 		],
 		"live": [
-			{"name": "live_model", "desc": "直播模型", "app": "live", "ids":[104708381],"factor_map":{"live_model":"LiveModelV1_2", "new_score": "0.5"}}
+			{"name": "live_model", "desc": "直播模型", "app": "live", "ids":[104708381],"factor_map":{"live_model":"LiveModelV1_2", "new_score": "0.7"}}
 		]
 	}`
 	if err := json.Unmarshal(([]byte)(white_config), &whiteListMap); err != nil {
