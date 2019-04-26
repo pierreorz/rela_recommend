9a529c458a93378fb91c94da2dfb03036a2d195e
rela <rela@ArvindeMBP.lan>
2019-04-26 11:06:32 +0800

    update live model

--- service/abtest/listen.go
+++ service/abtest/listen.go
@@ -51,7 +51,7 @@ func init() {
 			{"name": "match_model", "desc": "匹配模型", "app": "match", "ids":[104708381],"factor_map":{"match_model":"QuickMatchTreeV1_4"}}
 		],
 		"live": [
-			{"name": "live_model", "desc": "直播模型", "app": "live", "ids":[104708381],"factor_map":{"live_model":"LiveModelV1_2", "new_score": "0.3"}}
+			{"name": "live_model", "desc": "直播模型", "app": "live", "ids":[104708381],"factor_map":{"live_model":"LiveModelV1_3", "new_score": "0.3"}}
 		]
 	}`
 	if err := json.Unmarshal(([]byte)(white_config), &whiteListMap); err != nil {
