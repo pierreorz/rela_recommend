a131127665ab92ecd82c21d84ccc56d1beba3197
rela <rela@ArvindeMBP.lan>
2019-05-06 14:11:02 +0800

    update live model

--- service/abtest/listen.go
+++ service/abtest/listen.go
@@ -12,7 +12,7 @@ func init() {
 	// 因子默认值
 	default_config := `{
 		"match": {"match_model": "QuickMatchTreeV1_0", "default_key": "test"},
-		"live": {"live_model": "LiveModelV1_2", "new_score": "0.3"}
+		"live": {"live_model": "LiveModelV1_3", "new_score": "0.5"}
 	}`
 	if err := json.Unmarshal(([]byte)(default_config), &defaultFactorMap); err != nil {
 		log.Error(err.Error())
