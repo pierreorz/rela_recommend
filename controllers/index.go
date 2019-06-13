6f9e55871eab24dd93a8cec168de8bbda8442e49
rela <rela@ArvindeMacBook-Pro.local>
2019-06-13 18:08:33 +0800

    add new

--- controllers/index.go
+++ controllers/index.go
@@ -5,6 +5,10 @@ import (
 	"rela_recommend/service"
 	"rela_recommend/algo"
 	"rela_recommend/utils/response"
+
+	_ "rela_recommend/algo/moment"
+	_ "rela_recommend/algo/moment/coarse"
+	_ "rela_recommend/algo/theme"
 )
 
 func IndexHTTP(c *routers.Context) {
