d44a50ba608740a3c775b2b12680981a63657a7e
rela <rela@ArvindeMacBook-Pro.local>
2019-07-09 13:58:21 +0800

    rpc err

--- rpc/base.go
+++ rpc/base.go
@@ -25,105 +25,55 @@ func NewHttpClient(api_host string, timeout time.Duration) *HttpClient {
 	}
 }
 
-func (cli *HttpClient) SendPOSTJson(url string, body []byte, internalClientRes interface{}) error {
-	finalUrl := cli.api_host+url
-	req, _ := http.NewRequest(http.MethodPost, finalUrl, bytes.NewBuffer(body))
-	req.Header.Set("Content-Type", "application/json")
+func (cli *HttpClient) doRequest(req *http.Request, internalClientRes interface{}) error {
 	resp, err := cli.client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return errors.New(resp.Status)
+	if err == nil {
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			err = errors.New(resp.Status)
+		}
 	}
 	if internalClientRes == nil {
 		return nil
 	}
-	data, err := ioutil.ReadAll(resp.Body)
 	if err == nil {
-		err = json.Unmarshal(data, internalClientRes)
+		var data []byte
+		data, err = ioutil.ReadAll(resp.Body)
+		if err == nil {
+			err = json.Unmarshal(data, internalClientRes)
+		}
 	}
-
+	
 	if err != nil {
-		log.Errorf("SendPOSTJson err, %s %s\n", finalUrl, err)
+		log.Errorf("doRequest err, %s %s %s\n", req.Method, req.URL.String(), err)
 	}
 	return err
+}
+
+func (cli *HttpClient) SendPOSTJson(url string, body []byte, internalClientRes interface{}) error {
+	finalUrl := cli.api_host+url
+	req, _ := http.NewRequest(http.MethodPost, finalUrl, bytes.NewBuffer(body))
+	req.Header.Set("Content-Type", "application/json")
+	return cli.doRequest(req, internalClientRes)
 }
 
 func (cli *HttpClient) SendGETForm(url, params string, internalClientRes interface{}) error {
 	finalUrl := cli.api_host+url+"?"+params
 	req, _ := http.NewRequest(http.MethodGet, finalUrl, nil)
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	resp, err := cli.client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return errors.New(resp.Status)
-	}
-	if internalClientRes == nil {
-		return nil
-	}
-	data, err := ioutil.ReadAll(resp.Body)
-	if err == nil {
-		err = json.Unmarshal(data, internalClientRes)
-	}
-
-	if err != nil {
-		log.Errorf("SendGETForm err, %s %s\n", finalUrl, err)
-	}
-	return err
+	return cli.doRequest(req, internalClientRes)
 }
 
 func (cli *HttpClient) SendPOSTForm(url string, body []byte, internalClientRes interface{}) error {
 	finalUrl := cli.api_host+url
 	req, _ := http.NewRequest(http.MethodPost, finalUrl, bytes.NewBuffer(body))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	resp, err := cli.client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return errors.New(resp.Status)
-	}
-	if internalClientRes == nil {
-		return nil
-	}
-	data, err := ioutil.ReadAll(resp.Body)
-	if err == nil {
-		err = json.Unmarshal(data, internalClientRes)
-	}
-
-	if err != nil {
-		log.Errorf("SendPOSTForm err, %s %s\n", finalUrl, err)
-	}
-	return err
+	return cli.doRequest(req, internalClientRes)
 }
 
 func (cli *HttpClient) SendGetHeader(url, headerKey, headerVal string, internalClientRes interface{}) error {
 	finalUrl := cli.api_host+url
 	req, _ := http.NewRequest(http.MethodGet, finalUrl, nil)
 	req.Header.Set(headerKey, headerVal)
-	resp, err := cli.client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return errors.New(resp.Status)
-	}
-	if internalClientRes == nil {
-		return nil
-	}
-	data, err := ioutil.ReadAll(resp.Body)
-	if err == nil {
-		err = json.Unmarshal(data, internalClientRes)
-	}
-	if err != nil {
-		log.Errorf("SendGetHeader err, %s %s\n", finalUrl, err)
-	}
-	return err
+	return cli.doRequest(req, internalClientRes)
 }
