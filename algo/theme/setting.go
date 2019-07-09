18306e52033dd57115f29c954e9d02f1ff538f8b
rela <rela@ArvindeMacBook-Pro.local>
2019-07-09 12:16:04 +0800

    rank routers

--- algo/theme/setting.go
+++ algo/theme/setting.go
@@ -33,16 +33,3 @@ var _ = algo.AddAppInfo(&algo.AppInfo{
 	SorterKey: "sorter", SorterDefault: "base", SorterMap: sorterMap,
 	PagerKey: "pager", PagerDefault: "base", PagerMap: pagerMap,
 	LoggerKey: "loggers", LoggerDefault: "features,performs", LoggerMap: loggerMap})
-
-// 话题推荐热门列表
-var _ = algo.AddAppInfo(&algo.AppInfo{
-	Name: "theme.hot", Path: workDir,
-	AlgoKey: "model", AlgoDefault: "model_base", AlgoMap: algosMap,
-	BuilderKey: "build", BuilderDefault: "base", BuilderMap: builderMap,
-	StrategyKey: "strategies", StrategyDefault: "", StrategyMap: strategyMap,
-	SorterKey: "sorter", SorterDefault: "base", SorterMap: sorterMap,
-	PagerKey: "pager", PagerDefault: "base", PagerMap: pagerMap,
-	LoggerKey: "loggers", LoggerDefault: "features,performs", LoggerMap: loggerMap})
-
-
-
