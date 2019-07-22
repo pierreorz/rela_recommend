ea3232261f47352d59039b81c887adf15b04220c
rela <rela@ArvindeMacBook-Pro.local>
2019-07-22 15:56:45 +0800

    realtime

--- algo/theme/strategies.go
+++ algo/theme/strategies.go
@@ -37,14 +37,15 @@ func (self *UserBehaviorStrategy) Do(ctx algo.IContext) error {
 		if behavior != nil {
 			if behavior.IsListExposured() {
 				// 展示次数
-				countRate := 2.0 / (1 + math.Exp(-float64(behavior.ListExposure.Count / avgCount))) -1
+				count2 := float64(behavior.ListExposure.Count / avgCount)
+				countRate := count2 / (1 + count2)
 				clickRate := behavior.ListClickRate()
 				if clickRate <= 0.000001 {	// 没有点击直接降权
-					timeSpc := 1 / (1 + math.Abs(float64(currTime - behavior.ListExposure.LastTime)) / 600.0)
+					timeSpc := 1 / (1 + math.Abs(float64(currTime - behavior.ListExposure.LastTime)) / 60.0)
 					upperRate = -float32(countRate) * float32(timeSpc)
 				} else {
 					timeSpc := 1 / (1 + math.Abs(float64(currTime - behavior.ListClick.LastTime)) / 600.0)
-					upperRate = behavior.ListClickRate() * float32(countRate) * float32(timeSpc)
+					upperRate = clickRate * float32(countRate) * float32(timeSpc)
 				}
 			}
 		}
