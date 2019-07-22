61afebe702ac24deae5807647bb85dd19f01f1c3
rela <rela@ArvindeMacBook-Pro.local>
2019-07-22 12:35:05 +0800

    realtime

--- algo/theme/strategies.go
+++ algo/theme/strategies.go
@@ -27,15 +27,17 @@ func DoUserBehaviorUpper(ctx algo.IContext, index int) error {
 	dataInfo := ctx.GetDataByIndex(index).(*DataInfo)
 	rankInfo := dataInfo.GetRankInfo()
 
-	var avgCount float32 = 10
+	var avgCount float32 = 5
 	var upperRate float32 = 1.0
 	behavior := dataInfo.ThemeBehavior
 	if behavior != nil {
 		if behavior.IsListExposured() {
+			// 展示次数
 			countRate := 2.0 / (1 + math.Exp(-float64(behavior.ListExposure.Count / avgCount))) -1
 			clickRate := behavior.ListClickRate()
 			if clickRate <= 0.000001 {	// 没有点击直接降权
-				upperRate = -float32(countRate)
+				behavior.ListExposure.LastTime
+				upperRate = -float32(countRate) 
 			} else {
 				upperRate = behavior.ListClickRate() * float32(countRate)
 			}
@@ -43,4 +45,36 @@ func DoUserBehaviorUpper(ctx algo.IContext, index int) error {
 	}
 	rankInfo.Score = rankInfo.Score * (1.0 + upperRate)
 	return nil
+}
+
+
+// 对自己的行为进行权重处理
+type UserBehaviorStrategy struct { }
+func (self *UserBehaviorStrategy) Do(ctx algo.IContext) error {
+	var err error
+	var avgCount float32 = 5
+	var upperRate float32 = 0.5
+	var currTime = float32(ctx.GetCreateTime().Unix())
+	for index := 0; index < ctx.GetDataLength(); index++ {
+		dataInfo := ctx.GetDataByIndex(index).(*DataInfo)
+		rankInfo := dataInfo.GetRankInfo()
+
+		behavior := dataInfo.ThemeBehavior
+		if behavior != nil {
+			if behavior.IsListExposured() {
+				// 展示次数
+				countRate := 2.0 / (1 + math.Exp(-float64(behavior.ListExposure.Count / avgCount))) -1
+				clickRate := behavior.ListClickRate()
+				if clickRate <= 0.000001 {	// 没有点击直接降权
+					timeSpc := 1 / (1 + math.Abs(float64(currTime - behavior.ListExposure.LastTime)) / 300.0)
+					upperRate = -float32(countRate) * float32(timeSpc)
+				} else {
+					timeSpc := 1 / (1 + math.Abs(float64(currTime - behavior.ListClick.LastTime)) / 300.0)
+					upperRate = behavior.ListClickRate() * float32(countRate) * float32(timeSpc)
+				}
+			}
+		}
+		rankInfo.Score = rankInfo.Score * (1.0 + upperRate)
+	}
+	return err
 }
