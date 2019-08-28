8c3287849e46aa49cb680e2fc34038187c6670ab
rela <rela@ArvindeMacBook-Pro.local>
2019-08-28 15:28:46 +0800

    moment recommend hot

--- service/abtest/listen.go
+++ service/abtest/listen.go
@@ -65,7 +65,7 @@ func init() {
 			{"name": "theme_behavior", "desc": "话题行为", "app": "theme", "ids":[104708381, 524],"factor_map":{"user_behavior_upper_switch":"false"}}
 		],
 		"moment": [
-			{"name": "moment_hot", "desc": "热门日志", "app": "moment", "ids":[104708381],"factor_map":{"new_moment_len":"300", "new_moment_offset_second": "86400", "recommend_list_key": "moment_recommend_list:%d","strategy:time_level:weight": "-1"}}
+			{"name": "moment_hot", "desc": "热门日志", "app": "moment", "ids":[104708381, 524, 104205897],"factor_map":{"new_moment_len":"200", "new_moment_offset_second": "86400", "recommend_list_key": "moment_recommend_list:%d","strategy:time_level:weight": "-1"}}
 		]
 	}`
 	if err := json.Unmarshal(([]byte)(white_config), &whiteListMap); err != nil {
