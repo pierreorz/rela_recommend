ebc7e6861d8998ef542e64101942add106c1dd71
rela <rela@ArvindeMacBook-Pro.local>
2019-11-14 16:19:37 +0800

    new match

--- algo/user/strategies.go
+++ algo/user/strategies.go
@@ -8,10 +8,31 @@ import (
 
 func SortWithDistanceItem(ctx algo.IContext, iDataInfo algo.IDataInfo, rankInfo *algo.RankInfo) error {
 	request := ctx.GetRequest()
+	abtest := ctx.GetAbTest()
 	dataInfo := iDataInfo.(*DataInfo)
 	dataLocation := dataInfo.UserCache.Location
 
 	distance := rutils.EarthDistance(float64(request.Lng), float64(request.Lat), dataLocation.Lon, dataLocation.Lat)
-	rankInfo.Score = -float32(distance)
+	if abtest.GetBool("sort_with_distance", true) {
+		rankInfo.Score = -float32(distance)
+	} else {	// 安装距离分段排序
+		if distance < 1000 {
+			rankInfo.Level = 7
+		} else if distance < 3000 {
+			rankInfo.Level = 6
+		} else if distance < 5000 {
+			rankInfo.Level = 5
+		} else if distance < 10000 {
+			rankInfo.Level = 4
+		} else if distance < 30000 {
+			rankInfo.Level = 3
+		} else if distance < 50000 {
+			rankInfo.Level = 2
+		} else if distance < 100000 {
+			rankInfo.Level = 1
+		} else {
+			rankInfo.Level = 0
+		}
+	}
 	return nil
 }
