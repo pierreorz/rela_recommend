be4e8f2284f330048238f5c429690304ef94e204
rela <rela@ArvindeMacBook-Pro.local>
2019-11-18 12:26:11 +0800

    match build

--- algo/match/build.go
+++ algo/match/build.go
@@ -30,17 +30,27 @@ func DoBuildData(ctx algo.IContext) error {
 		log.Warnf("users cache list is err, %s\n", userCacheErr)
 	}
 
+	// 获取画像信息
+	var startProfileTime = time.Now()
+	matchUser, matchUserMap, matchCacheErr := userCache.QueryMatchProfileByUserAndUsersMap(params.UserId, dataIds)
+	if matchCacheErr != nil {
+		log.Warnf("match profile cache list is err, %s\n", matchCacheErr)
+	}
+
+	// 生成数据
 	var startBuildTime = time.Now()
 
 	userInfo := &UserInfo{
 		UserId:    params.UserId,
-		UserCache: user}
+		UserCache: user,
+		MatchProfile: matchUser,}
 
 	dataList := make([]algo.IDataInfo, 0)
 	for dataId, data := range usersMap {
 		info := &DataInfo{
 			DataId:    dataId,
 			UserCache: data,
+			MatchProfile: matchUserMap[dataId],
 			RankInfo:  &algo.RankInfo{},
 		}
 		dataList = append(dataList, info)
@@ -50,9 +60,10 @@ func DoBuildData(ctx algo.IContext) error {
 	ctx.SetDataList(dataList)
 	var endTime = time.Now()
 
-	log.Infof("rankid:%s,totallen:%d,cache:%d;total:%.3f,dataids:%.3f,cache:%.3f,build:%.3f\n",
+	log.Infof("rankid:%s,totallen:%d,cache:%d;total:%.3f,dataids:%.3f,user_cache:%.3f,profile_cache:%.3f,build:%.3f\n",
 		ctx.GetRankId(), len(dataIds), len(dataList),
-		endTime.Sub(startTime).Seconds(), startUserTime.Sub(startTime).Seconds(),
-		startBuildTime.Sub(startUserTime).Seconds(), endTime.Sub(startBuildTime).Seconds())
+		endTime.Sub(startTime).Seconds(), startUserTime.Sub(startTime).Seconds(), 
+		startProfileTime.Sub(startUserTime).Seconds(),
+		startBuildTime.Sub(startProfileTime).Seconds(), endTime.Sub(startBuildTime).Seconds())
 	return err
 }
--- models/redis/match.go
+++ models/redis/match.go
@@ -17,3 +17,30 @@ type MatchProfile struct {
 	ContinuesUse  int64              `json:"continues_use"`
 	// TimestampMap  map[string]float32 `json:"timestamp"`
 }
+
+
+// 读取速配画像信息
+func (self *UserCacheModule) QueryMatchProfileByIds(ids []int64) ([]MatchProfile, error) {
+	keyFormatter := "match_user_profile:%d"
+	ress, err := self.MGetStructs(MatchProfile{}, ids, keyFormatter, 24 * 60 * 60, 60 * 60 * 1)
+	objs := ress.Interface().([]MatchProfile)
+	return objs, err
+}
+
+// 获取当前用户和用户列表Map
+func (this *UserCacheModule) QueryMatchProfileByUserAndUsersMap(userId int64, userIds []int64) (*MatchProfile, map[int64]*MatchProfile, error) {
+	allIds := append(userIds, userId)
+	users, err := this.QueryMatchProfileByIds(allIds)
+	var resUser *MatchProfile
+	var resUsersMap = make(map[int64]*MatchProfile, 0)
+	if err == nil {
+		for _, user := range users {
+			if user.UserID == userId {
+				resUser = &user
+			} else {
+				resUsersMap[user.UserID] = &user
+			}
+		}
+	}
+	return resUser, resUsersMap, err
+}
