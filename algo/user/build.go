8fc01f31e913d6b418f115e0fd2e2c3bc9ad006c
rela <rela@MacBook-Pro.lan>
2019-12-11 16:09:29 +0800

    new performs

--- algo/user/build.go
+++ algo/user/build.go
@@ -13,6 +13,7 @@ func DoBuildData(ctx algo.IContext) error {
 	var err error
 	var startTime = time.Now()
 	// abtest := ctx.GetAbTest()
+	pf := ctx.GetPerforms()
 	params := ctx.GetRequest()
 	userCache := redis.NewUserCacheModule(ctx, &factory.CacheCluster, &factory.PikaCluster)
 	
@@ -24,20 +25,25 @@ func DoBuildData(ctx algo.IContext) error {
 
 	// 获取用户信息
 	var startUserTime = time.Now()
+	pf.Begin("user")
 	user, usersMap, userCacheErr := userCache.QueryByUserAndUsersMap(params.UserId, dataIds)
 	if userCacheErr != nil {
 		log.Warnf("users cache list is err, %s\n", userCacheErr)
 	}
+	pf.End("user")
 
 	// 获取画像信息
 	var startProfileTime = time.Now()
+	pf.Begin("profile")
 	userProfile, userProfileMap, profileCacheErr := userCache.QueryNearbyProfileByUserAndUsersMap(params.UserId, dataIds)
 	if profileCacheErr != nil {
 		log.Warnf("match profile cache list is err, %s\n", profileCacheErr)
 	}
+	pf.End("profile")
 
 	var startBuildTime = time.Now()
 
+	pf.Begin("build")
 	userInfo := &UserInfo{
 		UserId: params.UserId,
 		UserCache: user,
@@ -56,6 +62,7 @@ func DoBuildData(ctx algo.IContext) error {
 	ctx.SetUserInfo(userInfo)
 	ctx.SetDataIds(dataIds)
 	ctx.SetDataList(dataList)
+	pf.End("build")
 	var endTime = time.Now()
 
 	log.Infof("userId:%d,rankid:%s,totallen:%d,cache:%d;total:%.3f,dataids:%.3f,user_cache:%.3f,profile_cache:%.3f,build:%.3f\n",
