11280e92d1b5138f1bd0814ab4efb00702f6f3ba
rela <rela@MacBook-Pro.local>
2019-12-20 15:37:40 +0800

    user nearby live status

--- algo/base/strategy/utils.go
+++ algo/base/strategy/utils.go
@@ -22,7 +22,7 @@ func wilsonScoreForBernoulli(pos, total, z float64) float64 {
 func WilsonScore(expBe *behavior.Behavior, actBe *behavior.Behavior, scale float64) float64 {
 	if expBe != nil && expBe.Count > 0 && actBe != nil {
 		s := wilsonScoreForBernoulli(actBe.Count, expBe.Count, 2.0)
-		return math.Max(s * scale, 1.0)
+		return math.Min(s * scale, 1.0)
 	}
 	return 0.0
 }
