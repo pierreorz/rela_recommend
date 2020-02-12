e9e6141c5ddd794cc4a7ab27f0e7820918df2edb
王翔 <xiang.wang@rela.me>
2020-02-12 10:57:16 +0800

    willson

--- algo/moment/strategies.go
+++ algo/moment/strategies.go
@@ -1,6 +1,6 @@
 package moment
 
-import(
+import (
 	"rela_recommend/algo"
 	"rela_recommend/models/behavior"
 	"rela_recommend/algo/base/strategy"
@@ -9,7 +9,7 @@ import(
 // 按照6小时优先策略
 func DoTimeLevel(ctx algo.IContext, index int) error {
 	abtest := ctx.GetAbTest()
-	if hourStrategy:=abtest.GetInt("DoTimeLevel:time_interval",3); hourStrategy > 0{
+	if hourStrategy := abtest.GetInt("DoTimeLevel:time_interval", 3); hourStrategy > 0 {
 		dataInfo := ctx.GetDataByIndex(index).(*DataInfo)
 		rankInfo := dataInfo.GetRankInfo()
 		hours := int(ctx.GetCreateTime().Sub(dataInfo.MomentCache.InsertTime).Hours()) / hourStrategy
@@ -29,36 +29,47 @@ func DoTimeFirstLevel(ctx algo.IContext, index int) error {
 // 按数据被访问行为进行策略提降权
 func ItemBehaviorStrategyFunc(ctx algo.IContext, iDataInfo algo.IDataInfo, itembehavior *behavior.UserBehavior, rankInfo *algo.RankInfo) error {
 	var err error
+	var abTest = ctx.GetAbTest()
+
+	if abTest.GetBool("rich_strategy:moment_behavior:item_new", false) {
+		listRate := strategy.WilsonScore(itembehavior.GetThemeListExposure(), itembehavior.GetThemeListInteract(), 5)
+		infoRate := strategy.WilsonScore(itembehavior.GetThemeListExposure(), itembehavior.GetThemeListInteract(), 10)
+		upperRate := float32(listRate * 0.6 + infoRate * 0.4)
+
+		if upperRate != 0.0 {
+			rankInfo.AddRecommend("ItemBehaviorV1", 1.0 + upperRate)
+		}
+	} else{
 	var avgExpCount float64 = 50
 
 	listCountScore, listRateScore, listTimeScore := strategy.BehaviorCountRateTimeScore(
-		itembehavior.GetMomentListExposure(), itembehavior.GetMomentListInteract(), avgExpCount, 0, 0, 0)
+	itembehavior.GetMomentListExposure(), itembehavior.GetMomentListInteract(), avgExpCount, 0, 0, 0)
 
 	upperRate := float32(listCountScore * listRateScore * listTimeScore)
 
 	if upperRate != 0.0 {
-		rankInfo.AddRecommend("ItemBehavior", 1.0 + upperRate)
+	rankInfo.AddRecommend("ItemBehavior", 1.0 + upperRate)
+		}
 	}
 	return err
 }
-
 
 // 按用户访问行为进行策略提降权
 func UserBehaviorStrategyFunc(ctx algo.IContext, iDataInfo algo.IDataInfo, userbehavior *behavior.UserBehavior, rankInfo *algo.RankInfo) error {
 	var err error
 	var currTime = float64(ctx.GetCreateTime().Unix())
-	
+
 	if userbehavior != nil {
 		var avgExpCount float64 = 2
 
 		listCountScore, _, listTimeScore := strategy.BehaviorCountRateTimeScore(
-			userbehavior.GetMomentListExposure(), userbehavior.GetMomentListInteract(), 
+			userbehavior.GetMomentListExposure(), userbehavior.GetMomentListInteract(),
 			avgExpCount, currTime, 36000, 18000)
-		
+
 		upperRate := -float32(listCountScore * listTimeScore)
-		
+
 		if upperRate != 0.0 {
-			rankInfo.AddRecommend("UserBehavior", 1.0 + upperRate)
+			rankInfo.AddRecommend("UserBehavior", 1.0+upperRate)
 		}
 	}
 	return err
