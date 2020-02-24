e88b187879c7bb20d496640b64efd3252f43502a
Yongzhe Zhang <yongzhezhang@YongzhedeMacBook-Pro.local>
2020-02-24 13:35:21 +0800

    submit

--- algo/theme/strategies.go
+++ algo/theme/strategies.go
@@ -17,7 +17,7 @@ func ItemBehaviorStrategyFunc(ctx algo.IContext, iDataInfo algo.IDataInfo, itemb
 	if abTest.GetBool("rich_strategy:behavior:item_new", false) {
 		// 使用威尔逊算法估算内容情况：分值大概在0-0.2之间
 		listRate := strategy.WilsonScore(itembehavior.GetThemeListExposure(), itembehavior.GetThemeListInteract(), 3)
-		infoRate := strategy.WilsonScore(itembehavior.GetThemeListExposure(), itembehavior.GetThemeListInteract(), 10)
+		infoRate := strategy.WilsonScore(itembehavior.GetThemeListExposure(), itembehavior.GetThemeListInteract(), 8)
 		upperRate = float32(listRate * 0.6 + infoRate * 0.4)
 
 		if upperRate != 0.0 {
