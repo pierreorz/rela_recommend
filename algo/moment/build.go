a65dd152c8910237ce390eecfac6678afe051d36
王翔 <xiang.wang@rela.me>
2020-03-01 19:23:30 +0800

    embedding

--- algo/moment/build.go
+++ algo/moment/build.go
@@ -93,9 +93,9 @@ func DoBuildData(ctx algo.IContext) error {
 
 	var startEmbeddingTime = time.Now()
 	momentUserEmbedding, _, embeddingCacheErr := userCache.QueryMomentUserembeddingByUserAndUsersMap(params.UserId,userIds)
-	log.Info("user embedding id,%s\n",string(params.UserId))
-	log.Info("user embedding ids,%v\n",userIds)
-	log.Info("user embedding ids length,%d\n", len(userIds))
+	log.Infof("user embedding id,%s\n",string(params.UserId))
+	log.Infof("user embedding ids,%v\n",userIds)
+	log.Infof("user embedding ids length,%d",len(userIds))
 
 	if embeddingCacheErr != nil {
 		log.Warnf("moment user Embedding cache list is err, %s\n", embeddingCacheErr)
