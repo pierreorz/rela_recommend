9a1d246739192143b1e63045c82971554de69b0b
rela <rela@arvin-MacBook-Pro.local>
2020-04-09 21:08:10 +0800

    abtest upgrade

--- service/abtest/user.go
+++ service/abtest/user.go
@@ -5,12 +5,14 @@ import (
 )
 
 func GetUserAttr(userId int64, keys []string) map[string]interface{} {
-	res := map[string]interface{}{}
-	for _, key := range keys {
-		switch key {
-			case "vip_level": {	// 会员等级
-				vipRes, _ := api.CallUserVipStatus(userId)
-				res[key] = vipRes.Level
+	res := map[string]interface{}{"vip_level": 0}
+	if userId > 0 {
+		for _, key := range keys {
+			switch key {
+				case "vip_level": {	// 会员等级
+					vipRes, _ := api.CallUserVipStatus(userId)
+					res[key] = vipRes.Level
+				}
 			}
 		}
 	}
