a79f29f09d1ae91551c14e8ff66480d04e0d42f4
王翔 <xiang.wang@rela.me>
2020-04-27 10:11:26 +0800

    moment.near strategy

--- algo/moment/strategies.go
+++ algo/moment/strategies.go
@@ -12,10 +12,12 @@ func DoTimeLevel(ctx algo.IContext, index int) error {
 	abtest := ctx.GetAbTest()
 	dataInfo := ctx.GetDataByIndex(index).(*DataInfo)
 	rankInfo := dataInfo.GetRankInfo()
-	if hourStrategy := abtest.GetInt("DoTimeLevel:time_interval", 3); hourStrategy > 0 {
-		hours := int(ctx.GetCreateTime().Sub(dataInfo.MomentCache.InsertTime).Hours()) / hourStrategy
-		rankInfo.Level = -hours
+	if abtest.GetBool("sort_with_time", false) == false {
+		if hourStrategy := abtest.GetInt("DoTimeLevel:time_interval", 3); hourStrategy > 0 {
+			hours := int(ctx.GetCreateTime().Sub(dataInfo.MomentCache.InsertTime).Hours()) / hourStrategy
+			rankInfo.Level = -hours
 		}
+	}
 	return nil
 }
 
@@ -59,8 +61,19 @@ func ItemBehaviorStrategyFunc(ctx algo.IContext, iDataInfo algo.IDataInfo, itemb
 // 按用户访问行为进行策略提降权
 func UserBehaviorStrategyFunc(ctx algo.IContext, iDataInfo algo.IDataInfo, userbehavior *behavior.UserBehavior, rankInfo *algo.RankInfo) error {
 	var err error
-	var abtest=ctx.GetAbTest()
-	if abtest.GetBool("rich_strategy:behavior:moment_item_new", false){
+	var abtest = ctx.GetAbTest()
+	dataInfo := iDataInfo.(*DataInfo)
+	//非按时间序列排序，但是会对时间近的进行提权，前12h提权，后12h降权
+	sortWithTime := abtest.GetBool("sort_with_time", false)
+	if sortWithTime {
+		timeLevel := int(ctx.GetCreateTime().Sub(dataInfo.MomentCache.InsertTime).Hours()) / 3
+		if timeLevel <= 3 {
+			rankInfo.AddRecommend("momentNearTimeWeight", 1+float32(1/(2+timeLevel)))
+		} else {
+			rankInfo.AddRecommend("momentNearTimeWeight", float32(1/timeLevel-3))
+		}
+	}
+	if abtest.GetBool("rich_strategy:behavior:moment_item_new", false) {
 		if userbehavior != nil {
 			// 浏览过的内容使用浏览次数反序排列，3:未浏览过，2：浏览一次，1：浏览2次，0：浏览3次以上
 			allBehavior := behavior.MergeBehaviors(userbehavior.GetMomentListExposure(), userbehavior.GetMomentListInteract())
@@ -68,32 +81,26 @@ func UserBehaviorStrategyFunc(ctx algo.IContext, iDataInfo algo.IDataInfo, userb
 				rankInfo.Level = int(-math.Min(allBehavior.Count, 5))
 			}
 		}
-	}else{
+	} else {
 		var currTime = float64(ctx.GetCreateTime().Unix())
-		ifSort:=abtest.GetBool("sort_with_time",false)
-		//若按时间策略，会将日志分级别展示，0-6 6-12 12-18 18-24 每6h一级别
-		if ifSort{
-			dataInfo := iDataInfo.(*DataInfo)
-			rankInfo.Level=int(-ctx.GetCreateTime().Sub(dataInfo.MomentCache.InsertTime).Hours()) / 6
-		}
 		if userbehavior != nil {
-			if ifSort {
-				nearBehavior := behavior.MergeBehaviors(userbehavior.GetMomentNearListInteract(), userbehavior.GetMomentNearListExposure())
-				if nearBehavior != nil {
-					if nearBehaviorNum := int(math.Max(nearBehavior.Count, 1)); nearBehaviorNum%2 == 0 {
-						rankInfo.Level =- 4
-					}
+			if sortWithTime {
+				// 浏览过的内容使用浏览次数反序排列，3:未浏览过，2：浏览一次，1：浏览2次，0：浏览3次以上
+				NearBehavior := behavior.MergeBehaviors(userbehavior.GetMomentNearListExposure(), userbehavior.GetMomentNearListInteract())
+				if NearBehavior != nil {
+					rankInfo.Level = int(-math.Min(NearBehavior.Count, 3))
 				}
-			}
-			var avgExpCount float64 = 2
+			} else {
+				var avgExpCount float64 = 2
+				listCountScore, _, listTimeScore := strategy.BehaviorCountRateTimeScore(
+					userbehavior.GetMomentListExposure(), userbehavior.GetMomentListInteract(),
+					avgExpCount, currTime, 36000, 18000)
 
-			listCountScore, _, listTimeScore := strategy.BehaviorCountRateTimeScore(
-				userbehavior.GetMomentListExposure(), userbehavior.GetMomentListInteract(),
-				avgExpCount, currTime, 36000, 18000)
+				upperRate := -float32(listCountScore * listTimeScore)
 
-			upperRate := -float32(listCountScore * listTimeScore)
-
-			if upperRate != 0.0 {rankInfo.AddRecommend("UserBehavior", 1.0+upperRate)
+				if upperRate != 0.0 {
+					rankInfo.AddRecommend("UserBehavior", 1.0+upperRate)
+				}
 			}
 		}
 	}
