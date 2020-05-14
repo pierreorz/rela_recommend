d72ef33f1a8cf5ccf06f62908ed5ed4913799ea4
Kunyu Zhang <kunyuzhang@KunyudeMacBook-Pro.local>
2020-05-14 15:21:25 +0800

    change active user strategy

--- algo/match/strategies.go
+++ algo/match/strategies.go
@@ -5,20 +5,20 @@ import (
 	"time"
 )
 
-// 对24小时内活跃用户进行提权
+// 对在线活跃用户进行提权
 func ActiveUserUpperItem(ctx algo.IContext, iDataInfo algo.IDataInfo, rankInfo *algo.RankInfo) error {
-	var offsetTime int64 = 1 * 60 * 60
+	var offsetTime int64 = 0.5 * 60 * 60
 	nowTime := time.Now().Unix()
-	before24HourTime := nowTime - offsetTime
+	beforeTime := nowTime - offsetTime
 
 	dataInfo := iDataInfo.(*DataInfo)
 
-	after24hourTime := dataInfo.UserCache.LastUpdateTime - before24HourTime
-	if after24hourTime >= 0 {
+	afterTime := dataInfo.UserCache.LastUpdateTime - beforeTime
+	if afterTime >= 0 {
 		upperRate := ctx.GetAbTest().GetFloat("match_active_user_upper", 0.1)
 
-		var addRate = float32(after24hourTime) / float32(offsetTime) * upperRate
-		rankInfo.AddRecommend("ActiveUserUpper", addRate)
+		var addRate = float32(afterTime) / float32(offsetTime) * upperRate
+		rankInfo.AddRecommend("ActiveUserUpper", 1.0+addRate)
 	}
 	return nil
 }
