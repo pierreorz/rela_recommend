62b209abca941621684c6f8be955085822be785b
yongzhe.zhang@rela.onaliyun.com <>
2020-06-02 17:36:12 +0800

    submit

--- algo/theme/setting.go
+++ algo/theme/setting.go
@@ -12,7 +12,7 @@ var workDir = algo.GetWorkDir("/algo_files/theme/")
 var builderMap = map[string]algo.IBuilder{
 	"base": &algo.BuilderBase{DoBuild: DoBuildData},
 	"maybelike": &algo.BuilderBase{DoBuild: DoBuildMayBeLikeData},
-	"quick":&algo.BuilderBase{DoBuild: DoBuildQuickData}}
+	"quick":&algo.BuilderBase{DoBuild: DoBuildData}}
 var strategyMap = map[string]algo.IStrategy{}
 var sorterMap = map[string]algo.ISorter{
 	"base": &algo.SorterBase{}}
@@ -37,6 +37,10 @@ var algosMap = algo.AlgoListInitToMap([]algo.IAlgo{
 		Model: &utils.XgboostClassifier{}, FeaturesFunc: GetThemeFeaturesv0 },
 	&algo.AlgoBase{AlgoName: "model_theme_v2.1", FilePath: workDir + "mods_2.1.dumps.gz",
 		Model: &utils.XgboostClassifier{}, FeaturesFunc: GetThemeFeaturesv0 },
+})
+var algosQuickMap = algo.AlgoListInitToMap([]algo.IAlgo{
+	&algo.AlgoBase{AlgoName: "model_base", FilePath: workDir + "mods_1.0.dumps.gz",
+		Model: &utils.XgboostClassifier{}, FeaturesFunc: GetThemeFeatures },
 	&algo.AlgoBase{AlgoName: "model_theme_quick", FilePath: workDir + "mods_quick_2.1.dumps.gz",
 		Model: &utils.XgboostClassifier{}, FeaturesFunc: GetThemeQuickFeatures },
 })
@@ -88,7 +92,7 @@ var _ = algo.AddAppInfo(&algo.AppInfo{
 // 话题快捷列表
 var _ = algo.AddAppInfo(&algo.AppInfo{
 	Name: "theme.quick", Module: "theme", Path: workDir,
-	AlgoKey: "model", AlgoDefault: "model_quick", AlgoMap: nil,
+	AlgoKey: "model", AlgoDefault: "model_quick", AlgoMap: algosQuickMap,
 	BuilderKey: "build", BuilderDefault: "quick", BuilderMap: builderMap,
 	SorterKey: "sorter", SorterDefault: "base", SorterMap: sorterMap,
 	PagerKey: "pager", PagerDefault: "base", PagerMap: pagerMap,
