2bdea82fe0ca4a3b30a3d3fcb4a6c633d2048263
Kunyu Zhang <kunyuzhang@KunyudeMacBook-Pro.local>
2020-07-16 17:26:50 +0800

    add match filter condition

--- rpc/search/match.go
+++ rpc/search/match.go
@@ -66,6 +66,7 @@ func CallMatchList(userId int64, lat, lng float32, userIds []int64, user *redis.
 	strsIds := strings.Join(strIds, ",")
 
 	filters := []string{}
+	log.Infof("call search user:%+v", user)
 	if user != nil && user.WantRole != "" && user.WantRole != "0" {
 		filters = []string{
 			fmt.Sprintf("role_name:%s", user.WantRole),
