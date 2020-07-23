170b8794d2174f7efe76c5f1ff2717b6129f4b9f
arvin.mei <arvin.mei@rela.me>
2020-07-23 10:56:42 +0800

    performs run

--- service/performs/performs.go
+++ service/performs/performs.go
@@ -1,20 +1,22 @@
 package performs
 
 import (
-	"time"
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"rela_recommend/log"
+	"time"
 )
 
 type Performs struct {
-	Name 		string
-	BeginTime 	*time.Time
-	EndTime		*time.Time
-	IsEnd		bool
-	Interval 	float64
-	ItemsName	[]string
-	ItemsMap	map[string]*Performs
+	Name      string               `json:"name"`
+	BeginTime *time.Time           `json:"-"`
+	EndTime   *time.Time           `json:"-"`
+	IsEnd     bool                 `json:"-"`
+	Interval  float64              `json:"interval"`
+	Result    interface{}          `json:"result,omitempty"`
+	ItemsName []string             `json:"-"`
+	ItemsMap  map[string]*Performs `json:"items,omitempty"`
 }
 
 func (self *Performs) check() {
@@ -46,9 +48,9 @@ func (self *Performs) addChild(name string) *Performs {
 	}
 }
 
-func(self *Performs) findNext() *Performs {
+func (self *Performs) findNext() *Performs {
 	if length := self.Length(); length > 0 {
-		currName := self.ItemsName[length - 1]
+		currName := self.ItemsName[length-1]
 		if val, ok := self.ItemsMap[currName]; ok && !val.IsEnd {
 			return val
 		}
@@ -56,34 +58,46 @@ func(self *Performs) findNext() *Performs {
 	return nil
 }
 
-func (self *Performs) Begin(name string) {
+func (self *Performs) Begin(name string) *Performs {
 	self.check()
 	// 递归查找当前活跃级别，如果有就执行递归开始，如果没有就创建
 	if val := self.findNext(); val != nil {
-		val.Begin(name)
+		return val.Begin(name)
 	} else {
-		self.addChild(name)
+		return self.addChild(name)
 	}
 }
 
-func (self *Performs) End(name string) {
+func (self *Performs) End(name string) *Performs {
+	return self.EndWithResult(name, nil)
+}
+
+func (self *Performs) EndWithResult(name string, result interface{}) *Performs {
 	self.check()
 	// 递归查找当前活跃级别，如果下级有就执行递归结束，如果没有就创建
 	if val := self.findNext(); val != nil {
-		val.End(name)
+		return val.EndWithResult(name, result)
 	} else {
 		if self.Name == name {
 			self.IsEnd = true
+			self.Result = result
 		}
+		return self
 	}
 }
 
-func(self *Performs) EndAndBegin(endName string, beginName string) {
+func (self *Performs) EndAndBegin(endName string, beginName string) *Performs {
 	self.End(endName)
-	self.Begin(beginName)
+	return self.Begin(beginName)
 }
 
-func(self *Performs) toString(buffer *bytes.Buffer, pre string) {
+func (self *Performs) Run(name string, runFunc func() interface{}) *Performs {
+	self.Begin(name)
+	result := runFunc()
+	return self.EndWithResult(name, result)
+}
+
+func (self *Performs) toString(buffer *bytes.Buffer, pre string) {
 	fullName := pre + "." + self.Name
 	if pre == "" {
 		if self.Name == "" {
@@ -92,7 +106,13 @@ func(self *Performs) toString(buffer *bytes.Buffer, pre string) {
 			fullName = self.Name
 		}
 	}
-	buffer.WriteString(fmt.Sprintf("%s:%.3f,", fullName, self.Interval))
+	var slog string
+	if self.Result != nil {
+		slog = fmt.Sprintf("%s:%.3f:%v,", fullName, self.Interval, self.Result)
+	} else {
+		slog = fmt.Sprintf("%s:%.3f,", fullName, self.Interval)
+	}
+	buffer.WriteString(slog)
 	for _, name := range self.ItemsName {
 		if val, ok := self.ItemsMap[name]; ok {
 			val.toString(buffer, fullName)
@@ -100,9 +120,15 @@ func(self *Performs) toString(buffer *bytes.Buffer, pre string) {
 	}
 }
 
-func(self *Performs) ToString() string {
+func (self *Performs) ToString() string {
 	self.check()
 	var buffer = &bytes.Buffer{}
 	self.toString(buffer, "")
 	return buffer.String()
+}
+
+func (self *Performs) ToJson() string {
+	self.check()
+	jss, _ := json.Marshal(self)
+	return string(jss)
 }
