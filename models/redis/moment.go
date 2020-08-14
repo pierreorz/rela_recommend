b8546c8be8a64da22101f1cb63ca00c015ffcfb4
王翔 <xiang.wang@rela.me>
2020-08-14 11:50:50 +0800

    moment cache

--- models/redis/moment.go
+++ models/redis/moment.go
@@ -180,13 +180,8 @@ func (this *MomentCacheModule) QueryMomentOfflineProfileByIdsMap(momentIds []int
 
 // 读取直播相关用户画像
 func (self *MomentCacheModule) QueryMomentsByIds(ids []int64) ([]MomentsAndExtend, error) {
-<<<<<<< HEAD
 	keyFormatter := self.ctx.GetAbTest().GetString("moment_cache_key_formatter", "friends_moments_moments:%d")
 	ress, err := self.MGetStructs(MomentsAndExtend{}, ids, keyFormatter, 24 * 60 * 60, 60 * 60 * 1)
-=======
-	keyFormatter := "friends_moments_moments:%d"
-	ress, err := self.MGetStructs(MomentsAndExtend{}, ids, keyFormatter, 24*60*60, 60*60*1)
->>>>>>> master
 	objs := ress.Interface().([]MomentsAndExtend)
 	return objs, err
 }
