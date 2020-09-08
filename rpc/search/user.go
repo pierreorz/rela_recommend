98267e10fe6b255b954b86b2ec3b51995f081ae9
arvin.mei <arvin.mei@rela.me>
2020-09-08 19:52:38 +0800

    cache

--- rpc/search/user.go
+++ rpc/search/user.go
@@ -3,6 +3,7 @@ package search
 import (
 	"encoding/json"
 	"fmt"
+	"theL_api_golang/log"
 )
 
 const internalSearchNearUserListUrlV1 = "/search/nearby_user"
@@ -21,37 +22,39 @@ type apiSearchStruct struct {
 // 获取附近用户列表
 func CallNearUserIdList(userId int64, lat, lng float32, offset, limit int64, filterJson string) ([]int64, error) {
 	filters := []string{}
-	if len(filterJson) > 0 {
-		apiFilter := &apiSearchStruct{}
+	apiFilter := &apiSearchStruct{}
+	if len(filterJson) >= 2 { //解析json '{"a":"1"}'
 		if apiFilterErr := json.Unmarshal([]byte(filterJson), apiFilter); apiFilterErr != nil {
-			return []int64{}, apiFilterErr
-		} else {
-			//普通
-			if apiFilter.RoleName != "" { // 自我认同
-				filters = append(filters, fmt.Sprintf("role_name:%s", apiFilter.RoleName))
-			}
-			if apiFilter.Affection != "" { // 感情状态
-				filters = append(filters, fmt.Sprintf("affection:%s", apiFilter.Affection))
-			}
-			if apiFilter.MinAge != "" || apiFilter.MaxAge != "" { // 年龄范围
-				filters = append(filters, fmt.Sprintf("age:[%s,%s]", apiFilter.MinAge, apiFilter.MaxAge))
-			}
-			//会员特权
-			if apiFilter.ActiveDuration != "" { // 是否在线
-				filters = append(filters, fmt.Sprintf("activity_time:(now-%sm/m,)", apiFilter.ActiveDuration))
-			} else {
-				filters = append(filters, "activity_time:(now-7d/m,)")
-			}
-			if apiFilter.IsVip == "1" { //是否vip
-				filters = append(filters, fmt.Sprintf("vip_end_time:(now/m,)"))
-			}
-			if apiFilter.HasImage == "1" { //是否有图片
-				filters = append(filters, fmt.Sprintf("user_image_count:[1,)"))
-			}
-			if apiFilter.Horoscope != "" { // 星座多选，逗号分割
-				filters = append(filters, fmt.Sprintf("horoscope:%s", apiFilter.Horoscope))
-			}
+			log.Warnf("search CallNearUserIdList params error: %+v", apiFilterErr)
 		}
 	}
+
+	//普通
+	if apiFilter.RoleName != "" { // 自我认同
+		filters = append(filters, fmt.Sprintf("role_name:%s", apiFilter.RoleName))
+	}
+	if apiFilter.Affection != "" { // 感情状态
+		filters = append(filters, fmt.Sprintf("affection:%s", apiFilter.Affection))
+	}
+	if apiFilter.MinAge != "" || apiFilter.MaxAge != "" { // 年龄范围
+		filters = append(filters, fmt.Sprintf("age:[%s,%s]", apiFilter.MinAge, apiFilter.MaxAge))
+	}
+	//会员特权
+	if apiFilter.ActiveDuration != "" { // 是否在线
+		filters = append(filters, fmt.Sprintf("activity_time:(now-%sm/m,)", apiFilter.ActiveDuration))
+	} else { // 默认7天活跃
+		filters = append(filters, "activity_time:(now-7d/m,)")
+	}
+
+	if apiFilter.IsVip == "1" { //是否vip
+		filters = append(filters, fmt.Sprintf("vip_end_time:(now/m,)"))
+	}
+	if apiFilter.HasImage == "1" { //是否有图片
+		filters = append(filters, fmt.Sprintf("user_image_count:[1,)"))
+	}
+	if apiFilter.Horoscope != "" { // 星座多选，逗号分割
+		filters = append(filters, fmt.Sprintf("horoscope:%s", apiFilter.Horoscope))
+	}
+
 	return CallSearchIdList(internalSearchNearUserListUrlV1, userId, lat, lng, offset, limit, filters)
 }
