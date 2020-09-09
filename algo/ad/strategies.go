36cdf808520801b554deffcf9580d2c48245e259
arvin.mei <arvin.mei@rela.me>
2020-09-09 16:48:56 +0800

    ad

--- algo/ad/strategies.go
+++ algo/ad/strategies.go
@@ -5,6 +5,7 @@ import (
 	"rela_recommend/algo"
 	rutils "rela_recommend/utils"
 )
+
 // 内容较短，包含关键词的内容沉底
 func BaseScoreStrategyItem(ctx algo.IContext, iDataInfo algo.IDataInfo, rankInfo *algo.RankInfo) error {
 	abtest := ctx.GetAbTest()
@@ -12,25 +13,24 @@ func BaseScoreStrategyItem(ctx algo.IContext, iDataInfo algo.IDataInfo, rankInfo
 	sd := dataInfo.SearchData
 
 	var priceRate float64 = 1.0
-	if sd.Cpm > 0 {	// 以默认 10元为基准1，100元为2，2.16为0.5
+	if sd.Cpm > 0 { // 以默认 10元为基准1，100元为2，2.16为0.5
 		priceRate = math.Log(float64(sd.Cpm) + 1.0)
 	}
 
 	var cntRate float64 = 1.0
 	if sd.Exposure > 0 {
-		runningRate :=  float64(ctx.GetCreateTime().Unix() - sd.StartTime) / float64(sd.EndTime - sd.StartTime)
-		exposureRate := float64(sd.HistoryExposures) / float64(sd.Exposure)
+		runningRate := float64(ctx.GetCreateTime().Unix()-sd.StartTime) / float64(sd.EndTime-sd.StartTime)
+		exposureRate := float64(sd.HistoryExposures+1) / float64(sd.Exposure+1) // 加1防止0出现导致无穷大
 		cnt_z := abtest.GetFloat64("base_score_cnt_z", 20.0)
-		cntRate = math.Pow(runningRate / exposureRate, cnt_z)
+		cntRate = math.Pow(runningRate/exposureRate, cnt_z)
 	}
 
 	var clickRate float64 = 1.0
-	var weightRate float64 = 1.0 + float64(sd.Weight) / 100.0
+	var weightRate float64 = 1.0 + float64(sd.Weight)/100.0
 
 	rankInfo.Score = float32(priceRate * cntRate * clickRate * weightRate)
 	return nil
 }
-
 
 // 测试用户查看测试内容时置顶
 func TestUserTopStrategyItem(ctx algo.IContext, iDataInfo algo.IDataInfo, rankInfo *algo.RankInfo) error {
