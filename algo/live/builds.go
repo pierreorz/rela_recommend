c8ad5cd0d1bf38a04cc383694f1aa9785efa5945
arvin.mei <arvin.mei@rela.me>
2020-09-25 15:19:10 +0800

    live performs

--- algo/live/builds.go
+++ algo/live/builds.go
@@ -3,93 +3,90 @@ package live
 import (
 	"rela_recommend/algo"
 	"rela_recommend/factory"
-	"rela_recommend/log"
 	"rela_recommend/models/pika"
 	"rela_recommend/models/redis"
+	"rela_recommend/service/performs"
 	"rela_recommend/utils"
-	"time"
 )
 
 func DoBuildData(ctx algo.IContext) error {
 	var err error
-	var startTime = time.Now()
 	params := ctx.GetRequest()
-
+	pfms := ctx.GetPerforms()
 	// userCache := pika.NewUserProfileModule(&factory.CacheCluster, &factory.PikaCluster)
 	userCache := redis.NewUserCacheModule(ctx, &factory.CacheCluster, &factory.PikaCluster)
 	rdsPikaCache := redis.NewLiveCacheModule(ctx, &factory.CacheCluster, &factory.PikaCluster)
 	redisTheCache := redis.NewUserCacheModule(ctx, &factory.CacheRds, &factory.CacheRds)
 
 	var lives []pika.LiveCache
-	// 获取主播列表
-	liveType := utils.GetInt(params.Params["type"])
-	classify := utils.GetInt(params.Params["classify"])
-	lives = GetCachedLiveListByTypeClassify(liveType, classify)
+	var liveIds = []int64{}
+	pfms.Run("live", func(*performs.Performs) interface{} { // 获取主播列表
+		liveType := utils.GetInt(params.Params["type"])
+		classify := utils.GetInt(params.Params["classify"])
+		lives = GetCachedLiveListByTypeClassify(liveType, classify)
 
-	liveIds := make([]int64, len(lives))
-	for i, _ := range lives {
-		liveIds[i] = lives[i].Live.UserId
-	}
-	// 获取基础用户画像
-	startUserTime := time.Now()
-	user, users, err := userCache.QueryByUserAndUsers(params.UserId, liveIds)
-	if err != nil {
-		log.Warnf("QueryByUserAndUsers err: %s\n", err)
-	}
-	usersMap := make(map[int64]redis.UserProfile)
-	for i, _ := range users {
-		usersMap[users[i].UserId] = users[i]
-	}
-	// 获取刷新用户画像
-	startLiveProfileTime := time.Now()
-	user2, users2, err2 := rdsPikaCache.QueryLiveProfileByUserAndUsers(params.UserId, liveIds)
-	if err2 != nil {
-		log.Warnf("redis QueryLiveProfileByUserAndUsers err: %s\n", err2)
-	}
-	usersMap2 := make(map[int64]redis.LiveProfile)
-	for i, _ := range users2 {
-		usersMap2[users2[i].UserId] = users2[i]
-	}
-
-	// 获取关注信息
-	startConcernsTime := time.Now()
-	// concerns := make([]int64, 0)
-	concerns, err := redisTheCache.QueryConcernsByUser(params.UserId)
-	if err != nil {
-		log.Warnf("QueryConcernsByUser err: %s\n", err)
-	}
-
-	startBuildTime := time.Now()
-	livesInfo := make([]algo.IDataInfo, 0)
-	for i, _ := range lives {
-		liveInfo := LiveInfo{
-			UserId:    lives[i].Live.UserId,
-			LiveCache: &lives[i],
-			UserCache: nil, LiveProfile: nil,
-			RankInfo: &algo.RankInfo{}}
-		if liveUser, ok := usersMap[lives[i].Live.UserId]; ok {
-			liveInfo.UserCache = &liveUser
+		for i, _ := range lives {
+			liveIds = append(liveIds, lives[i].Live.UserId)
 		}
-		if liveUser2, ok := usersMap2[lives[i].Live.UserId]; ok {
-			liveInfo.LiveProfile = &liveUser2
+		return len(lives)
+	})
+
+	var user *redis.UserProfile
+	var usersMap = map[int64]*redis.UserProfile{}
+	var user2 *redis.LiveProfile
+	var usersMap2 = map[int64]*redis.LiveProfile{}
+	var concernsSet = &utils.SetInt64{}
+	pfms.RunsGo("cache", map[string]func(*performs.Performs) interface{}{
+		"user": func(*performs.Performs) interface{} { // 获取基础用户画像
+			var userErr error
+			user, usersMap, userErr = userCache.QueryByUserAndUsersMap(params.UserId, liveIds)
+			if userErr == nil {
+				return len(usersMap)
+			}
+			return userErr
+		},
+		"profile": func(*performs.Performs) interface{} { // 获取刷新用户画像
+			var userProfileErr error
+			user2, usersMap2, userProfileErr = rdsPikaCache.QueryLiveProfileByUserAndUsersMap(params.UserId, liveIds)
+			if userProfileErr == nil {
+				return len(usersMap2)
+			}
+			return userProfileErr
+		},
+		"concerns": func(*performs.Performs) interface{} { // 获取关注信息
+			if concerns, conErr := redisTheCache.QueryConcernsByUser(params.UserId); conErr == nil {
+				concernsSet = utils.NewSetInt64FromArray(concerns)
+				return concernsSet.Len()
+			} else {
+				return conErr
+			}
+		},
+	})
+
+	pfms.Run("build", func(*performs.Performs) interface{} {
+		livesInfo := make([]algo.IDataInfo, 0)
+		for i, _ := range lives {
+			liveId := lives[i].Live.UserId
+			liveInfo := LiveInfo{
+				UserId:      liveId,
+				LiveCache:   &lives[i],
+				UserCache:   usersMap[liveId],
+				LiveProfile: usersMap2[liveId],
+				RankInfo:    &algo.RankInfo{}}
+			livesInfo = append(livesInfo, &liveInfo)
 		}
-		livesInfo = append(livesInfo, &liveInfo)
-	}
 
-	userInfo := &UserInfo{
-		UserId: user.UserId, UserCache: &user,
-		LiveProfile:  &user2,
-		UserConcerns: utils.NewSetInt64FromArray(concerns)}
+		userInfo := &UserInfo{
+			UserId:       user.UserId,
+			UserCache:    user,
+			LiveProfile:  user2,
+			UserConcerns: concernsSet}
 
-	ctx.SetUserInfo(userInfo)
-	ctx.SetDataList(livesInfo)
+		ctx.SetUserInfo(userInfo)
+		ctx.SetDataIds(liveIds)
+		ctx.SetDataList(livesInfo)
 
-	var endTime = time.Now()
-	log.Infof("rankid %s,type:%s,totallen:%d,backendlen:%d;total:%.3f,live:%.3f,user:%.3f,profile:%.3f,concerns:%.3f,build:%.3f\n",
-		ctx.GetRankId(), params.Type, len(lives), len(users),
-		endTime.Sub(startTime).Seconds(), startUserTime.Sub(startTime).Seconds(),
-		startLiveProfileTime.Sub(startUserTime).Seconds(),
-		startConcernsTime.Sub(startLiveProfileTime).Seconds(),
-		startBuildTime.Sub(startConcernsTime).Seconds(), endTime.Sub(startBuildTime).Seconds())
-	return nil
+		return len(livesInfo)
+	})
+	return err
 }
--- models/redis/live.go
+++ models/redis/live.go
@@ -1,22 +1,19 @@
 package redis
 
-import(
+import (
 	"fmt"
-	"errors"
+	"rela_recommend/algo"
 	"rela_recommend/cache"
 	"rela_recommend/utils"
-	"rela_recommend/algo"
 )
-
 
 // 直播业务的画像
 type LiveProfile struct {
-	UserId 	int64		`json:"user_id"`
+	UserId int64 `json:"user_id"`
 	// 观看直播间时间的 embedding
-	LiveViewUserEmbedding []float32		`json:"live_view5_user"`		// 用户端
-	LiveViewLiveEmbedding []float32		`json:"live_view5_live"`		// 主播端
+	LiveViewUserEmbedding []float32 `json:"live_view5_user"` // 用户端
+	LiveViewLiveEmbedding []float32 `json:"live_view5_live"` // 主播端
 }
-
 
 type LiveCacheModule struct {
 	CachePikaModule
@@ -29,28 +26,23 @@ func NewLiveCacheModule(ctx algo.IContext, cache *cache.Cache, store *cache.Cach
 // 读取直播相关用户画像
 func (self *LiveCacheModule) QueryLiveProfileByUserIds(ids []int64) ([]LiveProfile, error) {
 	keyFormatter := "live_profile:%d"
-	ress, err := self.MGetStructs(LiveProfile{}, ids, keyFormatter, 24 * 60 * 60, 60 * 60 * 1)
+	ress, err := self.MGetStructs(LiveProfile{}, ids, keyFormatter, 24*60*60, 60*60*1)
 	objs := ress.Interface().([]LiveProfile)
 	return objs, err
 }
 
-
-func (self *LiveCacheModule) QueryLiveProfileByUserAndUsers(userId int64, userIds []int64) (LiveProfile, []LiveProfile, error) {
+func (self *LiveCacheModule) QueryLiveProfileByUserAndUsersMap(userId int64, userIds []int64) (*LiveProfile, map[int64]*LiveProfile, error) {
 	allIds := append(userIds, userId)
 	users, err := self.QueryLiveProfileByUserIds(allIds)
-	var resUser LiveProfile
-	var resUsers []LiveProfile
+	var resUser *LiveProfile
+	var resUsers = map[int64]*LiveProfile{}
 	if err == nil {
 		for i, user := range users {
 			if user.UserId == userId {
-				resUser = user
-				resUsers = append(users[:i], users[i+1:]...)
-				// users i后面的内容向前移动了一位，内容发上了改变，谨慎使用
-				break
+				resUser = &users[i]
+			} else {
+				resUsers[user.UserId] = &users[i]
 			}
-		}
-		if resUser.UserId == 0 {
-			err = errors.New("LiveProfile user is nil " + utils.GetString(userId))
 		}
 	}
 	return resUser, resUsers, err
@@ -58,7 +50,7 @@ func (self *LiveCacheModule) QueryLiveProfileByUserAndUsers(userId int64, userId
 
 // 从缓存中获取以逗号分割的字符串，并转化成int64. 如 keys11  1,2,3,4,5
 func (self *LiveCacheModule) GetInt64List(id int64, keyFormatter string) ([]int64, error) {
-	res, err := self.GetSet(fmt.Sprintf(keyFormatter, id), 24 * 60 * 60, 1 * 60 * 60)
+	res, err := self.GetSet(fmt.Sprintf(keyFormatter, id), 24*60*60, 1*60*60)
 	if err == nil {
 		return utils.GetInt64s(utils.GetString(res)), nil
 	}
