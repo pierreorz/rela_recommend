9086b5da03eddc7f197a1cb57866a064f5e6c0ad
王翔 <xiang.wang@rela.me>
2020-10-12 19:21:54 +0800

    tag name

--- algo/moment/strategies.go
+++ algo/moment/strategies.go
@@ -7,6 +7,7 @@ import (
 	"rela_recommend/algo/utils"
 	"rela_recommend/models/behavior"
 	"strings"
+	"rela_recommend/log"
 )
 
 // 按照6小时优先策略
@@ -261,11 +262,12 @@ func UserBehaviorInteractStrategyFunc(ctx algo.IContext) error {
 					var score float64 = 0.0
 					var count float64 = 0.0
 					for _, tag := range dataInfo.MomentProfile.Tags {
-						if userTag, ok := tagMap[tag.Id]; ok && userTag != nil && tag.Id != 23 && tag.Id != 24 {
+						if userTag, ok := tagMap[tag.Id]; ok && userTag != nil  {
 							rate := math.Max(math.Min(userTag.Count/userInteract.Count, 1.0), 0.0)
 							hour := math.Max(currTime-userTag.LastTime, 0.0) / (60 * 60)
 							score += utils.ExpLogit(rate) * math.Exp(hour)
 							count += 1.0
+							log.Infof("tagChinese,%s\n",tag.Name)
 							//log.Debugf("UserBehaviorInteractStrategyFunc:%d,rate:%f,hour:%f,score:%f,count:%f", tag.Id, rate, hour, score, count)
 						}
 					}
