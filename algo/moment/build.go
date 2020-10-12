81ea231c3269f9bb51431a92cb0988d486fbb660
王翔 <xiang.wang@rela.me>
2020-10-12 18:46:51 +0800

    remove emotion

--- algo/moment/build.go
+++ algo/moment/build.go
@@ -208,10 +208,12 @@ func DoBuildData(ctx algo.IContext) error {
 			if mom.Moments == nil || mom.MomentsExtend == nil {
 				continue
 			}
-			//搜索过滤开关
-			if filteredAudit {
-				if (mom.MomentsProfile != nil && mom.MomentsProfile.AuditStatus == 0)||(mom.MomentsProfile==nil) {
-					continue
+			//搜索过滤开关(运营推荐不管审核状态)
+			if _,ok :=searchMomentMap[mom.Moments.Id];!ok{
+				if filteredAudit {
+					if (mom.MomentsProfile != nil && mom.MomentsProfile.AuditStatus == 0)||(mom.MomentsProfile==nil)|| {
+						continue
+					}
 				}
 			}
 			if mom.Moments.ShareTo != "all" {
