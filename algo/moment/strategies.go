d9e4d3da331c5445eccd490e380a9fd1eac16a47
王翔 <xiang.wang@rela.me>
2020-10-13 15:27:40 +0800

    add log

--- algo/moment/strategies.go
+++ algo/moment/strategies.go
@@ -262,13 +262,13 @@ func UserBehaviorInteractStrategyFunc(ctx algo.IContext) error {
 					var score float64 = 0.0
 					var count float64 = 0.0
 					for _, tag := range dataInfo.MomentProfile.Tags {
-						if userTag, ok := tagMap[tag.Id]; ok && userTag != nil &&tag.Id!=23 {
+						if userTag, ok := tagMap[tag.Id]; ok && userTag != nil  {
 							rate := math.Max(math.Min(userTag.Count/userInteract.Count, 1.0), 0.0)
 							hour := math.Max(currTime-userTag.LastTime, 0.0) / (60 * 60)
 							score += utils.ExpLogit(rate) * math.Exp(-hour)
 							count += 1.0
 							log.Infof("tagChinese,%s\n",tag.Name)
-							//log.Debugf("UserBehaviorInteractStrategyFunc:%d,rate:%f,hour:%f,score:%f,count:%f", tag.Id, rate, hour, score, count)
+							log.Debugf("UserBehaviorInteractStrategyFunc:%d,rate:%f,hour:%f,score:%f,count:%f,userTag:%s", tag.Id, rate, hour, score, count,userTag)
 						}
 					}
 					if count > 0.0 && score > 0.0 {
