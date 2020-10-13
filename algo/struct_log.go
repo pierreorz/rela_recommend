5e567349f30dba00bc32ad5a8ec94419b90ed36f
arvin.mei <arvin.mei@rela.me>
2020-10-13 13:39:04 +0800

    request log

--- algo/struct_log.go
+++ algo/struct_log.go
@@ -25,20 +25,21 @@ type RecommendRequestLog struct {
 	Module  string            `json:"module"` // 对应abtest中的app
 	Limit   int64             `json:"limit"`
 	Offset  int64             `json:"offset"`
-	Ua      string            `json:"ua"`
-	Os      string            `json:"os"`
-	Version int               `json:"version"`
-	Lat     float32           `json:"lat"`
-	Lng     float32           `json:"lng"`
-	UserId  int64             `json:"user_id"`
-	DataIds []int64           `json:"data_ids"`
-	AbMap   map[string]string `json:"ab_map"`
-	Params  map[string]string `json:"params"`
+	Ua      string            `json:"ua,omitempty"`
+	Os      string            `json:"os,omitempty"`
+	Version int               `json:"version,omitempty"`
+	Lat     float32           `json:"lat,omitempty"`
+	Lng     float32           `json:"lng,omitempty"`
+	UserId  int64             `json:"user_id,omitempty"`
+	DataIds []int64           `json:"data_ids,omitempty"`
+	AbMap   map[string]string `json:"ab_map,omitempty"`
+	Params  map[string]string `json:"params,omitempty"`
 	// 返回
 	CreateTime time.Time `json:"create_time"`
 	RankId     string    `json:"rank_id"`
 	Returns    int       `json:"returns"`
-	performs   string    `json:"performs"`
+	Performs   string    `json:"performs"`
+	Error      string    `json:"error,omitempty"`
 }
 
 func (self *RecommendRequestLog) ToJson() string {
