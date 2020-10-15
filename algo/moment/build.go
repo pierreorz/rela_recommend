371be939868523f257163a137c9a5c74c5edcd4f
王翔 <xiang.wang@rela.me>
2020-10-15 19:36:44 +0800

    add tag hot list

--- algo/moment/build.go
+++ algo/moment/build.go
@@ -11,6 +11,7 @@ import (
 	"rela_recommend/rpc/search"
 	"rela_recommend/service/performs"
 	"rela_recommend/utils"
+	"rela_recommend/log"
 )
 
 func DoBuildData(ctx algo.IContext) error {
@@ -115,7 +116,9 @@ func DoBuildData(ctx algo.IContext) error {
 							tagList = append(tagList, key)
 						}
 					}
+					log.Debugf("tagList:%s\n",tagList)
 					tagLists, err := momentCache.GetInt64ListByIds(tagList, "friends_moments_moment_tag:%d")
+					log.Debugf("tagLists:%s\n",tagLists)
 					tagIdSet := utils.SetInt64{}
 					if err == nil {
 						for _, tagList := range tagLists {
