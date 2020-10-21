3e8ab17b08c8b4744d9799821999750e4621558f
yongzhezhang <yongzhe.zhang@rela.me>
2020-10-21 15:17:48 +0800

    submit

--- algo/theme/build_reply.go
+++ algo/theme/build_reply.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"rela_recommend/algo"
 	"rela_recommend/factory"
+	"rela_recommend/log"
 	"rela_recommend/rpc/search"
 	"rela_recommend/service/performs"
 	// "rela_recommend/models/pika"
@@ -65,6 +66,7 @@ func DoBuildReplyData(ctx algo.IContext) error {
 			return realtimeErr
 		},
 	})
+	log.Infof("tagList",tagList)
 	preforms.Run("tag_recommend", func(*performs.Performs) interface{} {
 		//根据实时行为数据召回池数据
 		if userBehavior!=nil {
@@ -76,14 +78,15 @@ func DoBuildReplyData(ctx algo.IContext) error {
 						for _, themeDict := range momentList {
 							replyIdList = append(replyIdList, themeDict.ReplyId)
 							themeIdList = append(themeIdList, themeDict.MomentId)
+							log.Infof("theme & themereply",themeDict.MomentId,themeDict.ReplyId)
 						}
-						return len(momentList)
 					}
 				}
+				return len(tagRecommends)
 			}
 			return tagErr
 		}
-		return userBehavior
+		return nil
 	})
 	searchScenery := "theme"
 	searchReplyMap := map[int64]search.SearchMomentAuditResDataItem{} // 话题参与对应的审核与置顶结果
