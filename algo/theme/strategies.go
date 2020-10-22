e57d31df0357ea1c1afc687f0859c4e314e505d2
yongzhezhang <yongzhe.zhang@rela.me>
2020-10-22 17:29:24 +0800

    submit

--- algo/theme/strategies.go
+++ algo/theme/strategies.go
@@ -110,6 +110,7 @@ func UserShortTagWegiht(ctx algo.IContext, index int) error {
 	rankInfo := dataInfo.GetRankInfo()
 	tagMapLine :=userData.ThemeUser
 	tagList := ""
+	log.Infof("tagMapLine & dataInfo.MomentOfflineProfile",tagMapLine,dataInfo.MomentOfflineProfile)
 	if tagMapLine!=nil && dataInfo.MomentOfflineProfile!=nil{
 		shortTagList := tagMapLine.AiTag.UserShortTag
 		ThemetagList := dataInfo.MomentOfflineProfile.AiTag
