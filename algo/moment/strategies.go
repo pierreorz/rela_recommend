d6d370ca1617b09f55a2bb2a0e482aa92e4d0ddd
王翔 <xiang.wang@rela.me>
2020-11-10 15:37:40 +0800

    better_user

--- algo/moment/strategies.go
+++ algo/moment/strategies.go
@@ -33,6 +33,16 @@ func DoTimeWeightLevelV2(ctx algo.IContext, index int) error {
 	return nil
 }
 
+//优质用户推荐策略
+func BetterUserMomAddWeight(ctx algo.IContext, index int) error{
+	dataInfo := ctx.GetDataByIndex(index).(*DataInfo)
+	rankInfo := dataInfo.GetRankInfo()
+	if dataInfo.UserCache!=nil{
+		rankInfo.AddRecommend("betterUserWeight", 1+float32(math.Sqrt(dataInfo.UserCache.Grade))*0.02)
+	}
+	return nil
+}
+
 //用户短期偏好提取
 func ShortPrefAddWeight(ctx algo.IContext, index int) error {
 	dataInfo := ctx.GetDataByIndex(index).(*DataInfo)
--- models/redis/user.go
+++ models/redis/user.go
@@ -35,6 +35,9 @@ type UserProfile struct {
 	Ratio          int      `json:"ratio"`
 	CreateTime     JsonTime `json:"createTime"`
 	Horoscope      string   `json:"horoscope"`
+	Reason     string `json:"reason"`
+	Grade      float64   `json:"grade"`
+
 
 	JsonRoleLike map[string]float32 `json:"jsonRoleLike"`
 	JsonAffeLike map[string]float32 `json:"jsonAffeLike"`
