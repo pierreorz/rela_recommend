ed0ce3d3d9471aed1ceea34435f182bd7ca778bd
yongzhezhang <yongzhe.zhang@rela.me>
2021-06-15 16:23:07 +0800

    top_info

--- algo/theme/build_reply.go
+++ algo/theme/build_reply.go
@@ -181,7 +181,6 @@ func DoBuildReplyData(ctx algo.IContext) error {
 						endDate := mom.MomentsProfile.ActivityInfo.ActivityEndTime
 						timeNow := time.Now().Unix()
 						if endDate < timeNow {
-							remove_list := []int64{}
 							remove_list = append(remove_list, mom.Moments.Id)
 						}
 					}
