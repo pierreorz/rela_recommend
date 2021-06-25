deea9f551d45db4bbce1d7bad9a0153716a938b4
王翔 <xiang.wang@rela.me>
2021-06-25 14:57:01 +0800

    top live increase exposure

--- algo/moment/strategies.go
+++ algo/moment/strategies.go
@@ -459,7 +459,7 @@ func topLiveIncreaseExposureFunc(ctx algo.IContext) error{
 	for index := 0; index < ctx.GetDataLength(); index++ {
 		dataInfo := ctx.GetDataByIndex(index).(*DataInfo)
 		rankInfo := dataInfo.GetRankInfo()
-		if dataInfo.UserCache!=nil &&dataInfo.UserCache.TopLive==1&&rankInfo.LiveIndex>0{
+		if dataInfo.UserCache!=nil &&dataInfo.UserCache.TopLive==0&&rankInfo.LiveIndex>0{
 			rankInfo.HopeIndex=1+interval*(rankInfo.LiveIndex-1)
 		}
 	}
