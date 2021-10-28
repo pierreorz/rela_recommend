8b1d5467a97b8bdf8fca3f8d7878eda8618d76ca
王翔 <xiang.wang@rela.me>
2021-10-28 09:52:36 +0800

    bussiness

--- algo/moment/strategies.go
+++ algo/moment/strategies.go
@@ -6,7 +6,6 @@ import (
 	"rela_recommend/algo"
 	"rela_recommend/algo/base/strategy"
 	"rela_recommend/algo/utils"
-	"rela_recommend/log"
 	"rela_recommend/models/behavior"
 	"rela_recommend/models/redis"
 	"strings"
@@ -674,7 +673,6 @@ func BussinessExposureFunc(ctx algo.IContext) error{
 	if len(bussinessIdList)>0{
 		choice =RandChoiceOne(bussinessIdList)
 	}
-	log.Warnf("bussiness id %s",choice)
 	rand.Seed(time.Now().UnixNano())
 	//var intNum =rand.Intn(10)
 	if choice != 0 {
@@ -683,7 +681,7 @@ func BussinessExposureFunc(ctx algo.IContext) error{
 			rankInfo := dataInfo.GetRankInfo()
 			if moms := dataInfo.MomentCache; moms != nil {
 				if moms.Id == choice{
-					rankInfo.HopeIndex =0
+					rankInfo.HopeIndex =5
 				}
 			}
 		}
