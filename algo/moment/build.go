ca0915230d85964da02a3e8e5e4c99c9fc432323
王翔 <xiang.wang@rela.me>
2021-12-10 15:33:47 +0800

    adtest

--- algo/moment/build.go
+++ algo/moment/build.go
@@ -172,6 +172,7 @@ func DoBuildData(ctx algo.IContext) error {
 			},"adLocation": func(*performs.Performs) interface{} {
 				var adLocationSearchErr error
 				adLocationList,adLocationSearchErr = search.CallAdMomentListV1(params.UserId)
+				log.Warnf("ad location list %s",adLocationList)
 				return adLocationSearchErr
 			},"better_user": func(*performs.Performs) interface{} {
 				var errBetterUser error
