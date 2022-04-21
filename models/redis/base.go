82a51392cadec4ea0678c2d230a40413d749d179
王翔 <xiang.wang@rela.me>
2022-04-21 10:23:03 +0800

    live

--- models/redis/base.go
+++ models/redis/base.go
@@ -444,7 +444,7 @@ func (this *UserCacheModule) ZmembersInt64List(userId int64, keyFormatter string
 	idstrs, err := help.ZrangeInt64s(this.cache,key,0,-1)
 	log.Warnf("concerns ids %s",err)
 	log.Warnf("concerns key %s",key)
-	log.Warnf("concerns key %s",this.cache)
+	log.Warnf("concerns key %s",this.cache.ZRange(key,0,-1))
 	userIds := make([]int64, 0)
 	if err == nil {
 		for _, idstr := range idstrs {
