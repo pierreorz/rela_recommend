b2003917a26c1fdb741abd98d9f4a19e9acfc553
王翔 <xiang.wang@rela.me>
2022-07-07 10:27:23 +0800

    add recommend reason

--- algo/moment/strategies.go
+++ algo/moment/strategies.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"rela_recommend/algo"
 	"rela_recommend/algo/base/strategy"
+	rutils "rela_recommend/utils"
 	"rela_recommend/algo/utils"
 	"rela_recommend/log"
 	"rela_recommend/models/behavior"
@@ -1145,6 +1146,29 @@ func GenerateRangeNum(min, max int) int {
 	return randNum
 }
 
+func AddRecommendReasonFunc(ctx algo.IContext) error {
+	params := ctx.GetRequest()
+	for index := 0; index < ctx.GetDataLength(); index++ {
+		dataInfo := ctx.GetDataByIndex(index).(*DataInfo)
+		rankInfo := dataInfo.GetRankInfo()
+		moms :=dataInfo.MomentExtendCache
+		if rankInfo.IsBussiness==1{
+			rankInfo.AddRecommendWithType("follow",1,algo.TypeYouFollow)
+		}
+		if moms!=nil{
+			lat :=moms.Lat
+			lng :=moms.Lng
+			if rutils.EarthDistance(float64(params.Lng), float64(params.Lat), lng, lat)<=50000{
+				rankInfo.AddRecommendWithType("nearby",1,algo.TypeNearby)
+			}
+		}
+
+		//if {
+		//	rankInfo.AddRecommendWithType("hot",1,algo.TypeHot)
+		//}//判断是否热门
+	}
+	return nil
+}
 
 func getFeedRecExposure(pageMap map[string]int) int{
 	result :=0
