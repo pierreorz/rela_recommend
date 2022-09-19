614d7f39428aca4ed7000cadee3a52c09b26b650
王翔 <xiang.wang@rela.me>
2022-09-19 18:26:05 +0800

    add label

--- algo/live/strategies.go
+++ algo/live/strategies.go
@@ -178,6 +178,11 @@ func HourRankRecommendFunc(ctx algo.IContext) error {
 				}
 				dataInfo.LiveData.AddLabel(&labelItem{
 					Style: CityLabel,
+					NewStyle:newStyle{
+						Font:       "",
+						Background: "https://static.rela.me/whitetag2",
+						Color:      "313333",
+					},
 					Title: multiLanguage{
 						Chs: "同城",
 						Cht: "同城",
