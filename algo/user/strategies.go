1dc66d7508265c06e824af573e53dff9f5f07a20
morry <yuan.wang@rela.me>
2023-04-06 17:41:44 +0800

    feat(ntxl): rank level by 30mins active

--- algo/user/strategies.go
+++ algo/user/strategies.go
@@ -158,7 +158,7 @@ func NtxlActiveDecayWeightFunc(ctx algo.IContext, iDataInfo algo.IDataInfo, rank
 		if userProfile.IsActive(1800) {
 			rankInfo.Level = 7
 			ratio := rutils.GaussDecay(float64(userProfile.ActiveInSeconds()), 0, 30*60, 3600)
-			if ratio > 0 {
+			if ratio >= 0 {
 				rankInfo.AddRecommendWithType("ActiveDecay", float32(ratio), algo.TypeActive)
 			}
 		}
@@ -175,7 +175,7 @@ func NtxNearbyDecayWeightFunc(ctx algo.IContext, iDataInfo algo.IDataInfo, rankI
 		if userProfile := dataInfo.UserCache; userProfile != nil {
 			distance := userProfile.Distance(currentLng, currentLat)
 			ratio := rutils.GaussDecay(distance, 0, 3000, 5000)
-			if ratio > 0 {
+			if ratio >= 0 {
 				rankInfo.AddRecommendWithType("NearbyUserDecay", float32(ratio), algo.TypeNearbyUser)
 			}
 		}
